875f61d4a2390766f2f9ef23856aeab4397a671a
kristian <kristian@purestyle.se>
2020-07-18 11:17:39 +0200

    remove user query

--- main.c
+++ main.c
@@ -37,17 +37,13 @@ int main() {
   int run = 1;
   char user_input[256];
   struct Table tables[10];
-  struct Node *top_node = malloc(sizeof(struct Node));
+  struct Node *top_node;
 
   while(run==1) {
+    top_node = malloc(sizeof(struct Node));
     printf("sql>\n");
     yyparse(top_node);
     printf("haj haj och %s\n", top_node->str);
     print_tree(top_node, 0);
-    printf("continue? y/n\n");
-    gets(user_input);
-    if(user_input[0] == 'n') {
-      run=0;
-    }
   }
 }
